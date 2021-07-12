18ff0749c1a19b825f528c4173c1febf1dabb5a0
MaurizioSicorello <51700288+MaurizioSicorello@users.noreply.github.com>
2021-07-12 17:56:16 +0200

    bootResults

--- MediationAnalysis.m
+++ MediationAnalysis.m
@@ -22,6 +22,6 @@ varRetained = pdm.dat.M_tilde*pdm.dat.Dt;
 sum(var(varRetained)/size(mm, 2))
 
 cd('..\Results')
-pdm2 = multivariateMediation(pdm, 'nPDM', 4, 'svd', 'bootPDM', 'Bsamp', 5, 'save2file', 'test.mat', 'returnbootsamples');
+pdm2 = multivariateMediation(pdm, 'nPDM', 4, 'svd', 'bootPDM', 'Bsamp', 10000, 'save2file', 'test.mat', 'returnbootsamples');
 
 
