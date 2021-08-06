01d487cc6a84c6b0e9327fbaa7fd77b6a848b525
MaurizioSicorello <51700288+MaurizioSicorello@users.noreply.github.com>
2021-08-06 14:59:17 +0200

    minuteMediationchanges

--- MediationAnalysis.m
+++ MediationAnalysis.m
@@ -15,11 +15,21 @@ for i=1:size(DF, 1)
     
 end
 
-% compute PDM for 
+% compute PDM for continuous CTQ
+% pdm = multivariateMediation(xx,yy,mm_cell, 'B', 20, 'svd', 'plots');
+% 
+% varRetained = pdm.dat.M_tilde*pdm.dat.Dt;
+% sum(var(varRetained)/size(mm, 2))
+% 
+% cd('..\Results')
+% pdm2 = multivariateMediation(pdm, 'nPDM', 4, 'svd', 'bootPDM', 'Bsamp', 5000, 'save2file', 'multimedBootResults_cont.mat', 'returnbootsamples');
+
+
+% compute PDM for dichotomous trauma
 pdm = multivariateMediation(xx,yy,mm_cell, 'B', 20, 'svd', 'plots');
 
 varRetained = pdm.dat.M_tilde*pdm.dat.Dt;
 sum(var(varRetained)/size(mm, 2))
 
 cd('..\Results')
-pdm2 = multivariateMediation(pdm, 'nPDM', 4, 'svd', 'bootPDM', 'Bsamp', 5000, 'save2file', 'multimedBootResults.mat', 'returnbootsamples');
+pdm2 = multivariateMediation(pdm, 'nPDM', 4, 'svd', 'bootPDM', 'Bsamp', 5000, 'save2file', 'multimedBootResults_cont.mat', 'returnbootsamples');
