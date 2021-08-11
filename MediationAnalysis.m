79fbaad3ebb4e64ae40d033cc9184ee69a5a45f7
MaurizioSicorello <51700288+MaurizioSicorello@users.noreply.github.com>
2021-08-11 11:22:29 +0200

    bootCat_Results

--- MediationAnalysis.m
+++ MediationAnalysis.m
@@ -6,7 +6,8 @@ DF = zscore(DF);
 
 xx = num2cell(DF(:,1));
 yy = num2cell(DF(:,2));
-mm = DF(:, 3:size(DF, 2));
+xx_cat = num2cell(DF(:,3));
+mm = DF(:, 4:size(DF, 2));
 mm_cell = cell(size(xx,1), 1);
 
 for i=1:size(DF, 1)
@@ -26,10 +27,10 @@ end
 
 
 % compute PDM for dichotomous trauma
-pdm = multivariateMediation(xx,yy,mm_cell, 'B', 20, 'svd', 'plots');
+pdm = multivariateMediation(xx_cat,yy,mm_cell, 'B', 20, 'svd', 'plots');
 
 varRetained = pdm.dat.M_tilde*pdm.dat.Dt;
 sum(var(varRetained)/size(mm, 2))
 
 cd('..\Results')
-pdm2 = multivariateMediation(pdm, 'nPDM', 4, 'svd', 'bootPDM', 'Bsamp', 5000, 'save2file', 'multimedBootResults_cont.mat', 'returnbootsamples');
+pdm2 = multivariateMediation(pdm, 'nPDM', 4, 'svd', 'bootPDM', 'Bsamp', 5000, 'save2file', 'multimedBootResults_cat.mat', 'returnbootsamples');
