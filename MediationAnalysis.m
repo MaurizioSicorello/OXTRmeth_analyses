7b4b94e6bdaf83298bfb2d24a52cfb5772e10549
Maurizio Sicorello <mauriziosicorello@gmail.com>
2021-09-09 11:48:59 +0200

    repeatMediation

--- MediationAnalysis.m
+++ MediationAnalysis.m
@@ -16,21 +16,46 @@ for i=1:size(DF, 1)
     
 end
 
-% compute PDM for continuous CTQ
-% pdm = multivariateMediation(xx,yy,mm_cell, 'B', 20, 'svd', 'plots');
-% 
-% varRetained = pdm.dat.M_tilde*pdm.dat.Dt;
-% sum(var(varRetained)/size(mm, 2))
-% 
-% cd('..\Results')
-% pdm2 = multivariateMediation(pdm, 'nPDM', 4, 'svd', 'bootPDM', 'Bsamp', 5000, 'save2file', 'multimedBootResults_cont.mat', 'returnbootsamples');
+%compute PDM for continuous CTQ
+pdm = multivariateMediation(xx,yy,mm_cell, 'B', 20, 'svd', 'plots');
 
-
-% compute PDM for dichotomous trauma
-pdm = multivariateMediation(xx_cat,yy,mm_cell, 'B', 20, 'svd', 'plots');
+t = multivariateMediation(pdm, 'nPDM', 4, 'svd')
 
 varRetained = pdm.dat.M_tilde*pdm.dat.Dt;
 sum(var(varRetained)/size(mm, 2))
 
 cd('..\Results')
-pdm2 = multivariateMediation(pdm, 'nPDM', 4, 'svd', 'bootPDM', 'Bsamp', 5000, 'save2file', 'multimedBootResults_cat.mat', 'returnbootsamples');
+pdmBoot = multivariateMediation(pdm, 'nPDM', 4, 'svd', 'bootPDM', 'Bsamp', 5000, 'save2file', 'multimedBootResults_cont.mat', 'returnbootsamples');
+
+
+% compute PDM for dichotomous trauma
+pdm2 = multivariateMediation(xx_cat,yy,mm_cell, 'B', 20, 'svd', 'plots');
+
+varRetained = pdm2.dat.M_tilde*pdm2.dat.Dt;
+sum(var(varRetained)/size(mm, 2))
+
+% cd('..\Results')
+% pdm2Boot = multivariateMediation(pdm, 'nPDM', 4, 'svd', 'bootPDM', 'Bsamp', 5000, 'save2file', 'multimedBootResults_cat.mat', 'returnbootsamples');
+
+
+cd('Results')
+
+modelCont = load('multimedBootResults_cont.mat')
+bootABcont = modelCont.out.boot.SamplesTheta{1}(3,:).*modelCont.out.boot.SamplesTheta{1}(4,:);
+modelCont.out.Theta{1}(4)
+quantile(bootABcont, 0.025)
+quantile(bootABcont, 0.975)
+plot(modelCont.out.Wfull{1})
+
+modelCat = load('multimedBootResults_cat.mat')
+bootABcat = modelCat.out.boot.SamplesTheta{1}(3,:).*modelCat.out.boot.SamplesTheta{1}(4,:);
+modelCat.out.Theta{1}(4)
+quantile(bootABcat, 0.025)
+quantile(bootABcat, 0.975)
+plot(modelCat.out.Wfull{1})
+
+t = table(modelCont.out.Wfull{1}, modelCat.out.Wfull{1},'VariableNames', {'Wcont', 'Wcat'})
+
+
+length(modelCont.out.Wfull{1})
+
--- OXTRstructure.R
+++ OXTRstructure.R
@@ -1,7 +1,13 @@
 
-# TO DO: (1) repeat CTQ analyses with binary variable 'ctq_dich01_multi01', (2) repeat mediation analysis with Martin's settings, 
-# (3) robustness check of PLS loading plot, (4) repeat unsupervised analyses for childen, (5) quantify overlap between cluster solutions
+# TO DO: (2) rerun mediation analysis for continuous CTQ in matlab
 # (6) Antrag fertigstellen
+
+# ideas for shinyapp:
+
+# variance compared to sensitivity, percent outliers, skew, p-value DMR, cluster assignment, PLS loadings, both for kids and mothers
+
+
+
 
 
 #################################
@@ -94,7 +100,7 @@ CpG_corr = cor(df_CpG_m, use = "pairwise.complete.obs")
 colnames(CpG_corr) <- NULL
 rownames(CpG_corr) <- substring(rownames(CpG_corr), 7)
 
-pdf(here("Figures", "corrPlot.pdf"))
+pdf(here("Figures", "corrPlot_mother.pdf"))
 corrplot::corrplot(CpG_corr, tl.pos = "d", tl.cex = 0.25, method = "color", type = "upper", tl.col = "black")
 dev.off()
 
@@ -181,10 +187,23 @@ dev.off()
 
 clustAssign <- cutree(hc, k = 3)
 
+# proportions of genetic sections represented in the three clusters
 df_hclustAssign <- data.frame(dfcodes, clustAssign) 
 round(table(df_hclustAssign[df_hclustAssign$clustAssign == 1, "segment2"])/sum(clustAssign == 1), 2)
 round(table(df_hclustAssign[df_hclustAssign$clustAssign == 2, "segment2"])/sum(clustAssign == 2), 2)
 round(table(df_hclustAssign[df_hclustAssign$clustAssign == 3, "segment2"])/sum(clustAssign == 3), 2)
+
+
+df_hclustAssign$DBSCANclust <- df_clustMem$res.fpc5.cluster
+df_hclustAssign$hclustRec <- ifelse(df_hclustAssign$clustAssign == 1, 
+                                    0,
+                                    ifelse(df_hclustAssign$clustAssign == 2, 
+                                           1,
+                                           2)
+                                    )
+
+round(sum(df_hclustAssign$DBSCANclust == df_hclustAssign$hclustRec)/nrow(df_hclustAssign), 2)
+
 
 
 #################################
@@ -325,8 +344,8 @@ outcome = df_outcomes$ctq_dich01_multi01
 predictors = df_CpG_m
 
 # create permutation samples [takes ~2h on my machine]
-plsPerm_all_CTQcat <- permutePLSnestedCV(df_outcomes$ctq_dich01_multi01, df_CpG_m, nrepeats = repeats, nfolds = folds, nperms = perms, classification = T)
-write.csv(plsPerm_all_CTQcat, here("Results", "plsPerm_all_CTQcat.csv"))
+# plsPerm_all_CTQcat <- permutePLSnestedCV(df_outcomes$ctq_dich01_multi01, df_CpG_m, nrepeats = repeats, nfolds = folds, nperms = perms, classification = T)
+# write.csv(plsPerm_all_CTQcat, here("Results", "plsPerm_all_CTQcat.csv"))
 
 # p-value
 df_CTQcat_perms <- read.csv(here("Results", "plsPerm_all_CTQcat.csv"))
@@ -360,6 +379,8 @@ ggplot(data = dfplot_CTQcat_loadings, aes(x = reorder(dfcodes.CpG, rep((1:nrow(d
   geom_path(group = 1, size = 0.8) +
   
   theme_classic() + ylim(-max(abs(pls_CTQcat_loadings))*1.5, max(abs(pls_CTQcat_loadings))*1.5) 
+
+ggsave(here("figures", "PLSloadings_catTrauma.pdf"), device = "pdf")
 
 
 
@@ -395,7 +416,8 @@ cor(dfplot_CTQ_loadings[dfplot_CTQ_loadings$dfcodes.segment2 != "exon3_transl", 
     dfplot_geneExpr_loadings[dfplot_geneExpr_loadings$dfcodes.segment2 != "exon3_transl", "loadings"])
 
 # create df for mediation via PLS components
-dfMediation <- data.frame(df_outcomes[!is.na(df_outcomes$Genexp_OXTR_mother), ], as.numeric(CTQ_finalModel$scores[!is.na(df_outcomes$Genexp_OXTR_mother)]), as.numeric(geneExpr_finalModel$scores))
+dfMediation <- data.frame(df_outcomes[!is.na(df_outcomes$Genexp_OXTR_mother), ], 
+                          as.numeric(CTQ_finalModel$scores[!is.na(df_outcomes$Genexp_OXTR_mother)]), as.numeric(geneExpr_finalModel$scores))
 names(dfMediation)[4:5] <- c("CTQ_CpG_scores", "GeneExpr_CpG_scores") 
 dfMediation <- as.data.frame(scale(dfMediation))
 cor(dfMediation)
@@ -406,320 +428,224 @@ write.csv(dfMediation_Mat, file = "Data/MediationData.csv", row.names = F)
 
 
 
+t = PLSnested_Genexpr$dat
 
 
 
 
-
-
-
-
-
-
-
-
-
-##################################
-# PLS MT2
 
 #################
-# gene expression
-df_MT2 <- df_CpG_m[, which(names(df_CpG_m) == "CpG_m_27"):which(names(df_CpG_m) == "CpG_m_53")]
-df_predExpr_MT2 = data.frame(df_outcomes$Genexp_OXTR_mother, df_MT2)
-names(df_predExpr_MT2)[1] <- "geneExpr"
-df_predExpr_MT2[,1] <- scale(df_predExpr_MT2[,1], scale = T, center = T)
-df_predExpr_MT2 <- df_predExpr_MT2[!is.na(df_predExpr_MT2$geneExpr), ]
+# check pattern with unrelated variable
 
+dfRandPlot <- data.frame(
+  matrix(
+    ncol = 3,
+    nrow = 0
+    )
+)
 
-GeneExpr_out <- as.data.frame(matrix(nrow = nfolds*repeats, ncol = 2))
-GeneExpr_out_foldwise <- as.data.frame(matrix(nrow = maxcomp, ncol = repeats*nfolds))
-plsGrid <- expand.grid(ncomp = seq(1, maxcomp))
-count = 0
-seed = 1000
-
-for(i in 1:repeats){
+for(i in 1:20){
   
-  set.seed(seed+count)
-  plsFolds <- createFolds(df_predExpr_MT2$geneExpr, k = nfolds)
+  set.seed(i+1000)
   
-  for(j in 1:nfolds){
-    
-    count = count+1
-    
-    trainSet <- df_predExpr_MT2[-plsFolds[[j]],]
-    testSet <- df_predExpr_MT2[plsFolds[[j]],]
-    
-    set.seed(seed+count)
-    plsOptComp <- train(geneExpr ~ ., 
-                        data = trainSet, 
-                        trControl = fitControl, 
-                        method = "pls", 
-                        metric = "RMSE",
-                        preProcess = c('scale', 'center'), 
-                        na.action = na.omit,
-                        tuneGrid = plsGrid)
-    
-    Rsquared <- 1 - sum((testSet$geneExpr - predict(plsOptComp, newdata = testSet))^2)/(var(testSet$geneExpr)*(nrow(testSet)-1))
-    
-    GeneExpr_out[count, 1] <- max(Rsquared, 0)
-    GeneExpr_out[count, 2] <- plsOptComp$bestTune$ncomp
-    GeneExpr_out_foldwise[, count] <- plsOptComp$results$Rsquared
-    
-  }
+  randOut <- rnorm(nrow(df_CpG_m))
+  dfPLSage <- data.frame(randOut, df_CpG_m)
+  
+  testModel = plsr(randOut ~ .,
+                   data = dfPLSage,
+                   ncomp = 1)
+  summary(testModel)
+  
+  pls_testModel_loadings = testModel$loadings[,1:ncol(testModel$loadings)]
+  dfplot_testModel_loadings <- data.frame(dfcodes$CpG, pls_testModel_loadings, dfcodes$segment2)
+  
+  dfRandPlot <- rbind(dfRandPlot, dfplot_testModel_loadings)
+  
 }
 
-mean(GeneExpr_out[,1])
+dfRandPlot$iteration <- rep(1:20, each = nrow(dfplot_testModel_loadings))
+dfRandPlot$dfcodes.segment2 <- factor(dfRandPlot$dfcodes.segment2, levels = unique(dfRandPlot$dfcodes.segment2))
 
+ggplot(data = dfRandPlot, aes(x = reorder(dfcodes.CpG, rep((1:nrow(dfRandPlot)), length.out = nrow(dfRandPlot))), y = pls_testModel_loadings, group = iteration, colour = factor(dfcodes.segment2))) +
+  geom_hline(yintercept=0, colour = "darkgrey") +
+  geom_line(size = 0.8) +
 
-##########
-# permutation test
-
-savePerm <- numeric(nperm)
-
-t1 <- Sys.time()
-
-for(h in 1:nperm){
+  theme_classic() + ylim(-max(abs(pls_testModel_loadings))*1.5, max(abs(pls_testModel_loadings))*1.5) + ylab("PLS Loadings") + xlab("OXTR CpG Site") + 
   
-  dfpredExpr_perm <- df_predExpr
-  dfpredExpr_perm$geneExpr <- sample(dfpredExpr_perm$geneExpr)
+  theme(axis.text.x=element_blank(), axis.ticks.x=element_line(colour = "black"), legend.title = element_blank()) +
+  scale_color_brewer(palette="Spectral")
+
+ggsave(here("figures", "PLSloadings_random.pdf"), device = "pdf")
+
+
+
+
+
+
+##################################################################
+# repeat unsupervised analyses for children
+
+# subset mother CpGs and convert to decimal
+df_CpG_k = df[,grepl("CpG_k", names(df))]
+df_CpG_k = as.data.frame(apply(df_CpG_k,2, function(x){as.numeric(sub("%", "", x, fixed=TRUE))/100}))
+df_CpG_k = df_CpG_k[complete.cases(df_CpG_k),]
+
+# load identifier of gene sections
+dfcodes = read.csv(here("Data", "OXTR_segmentCodes.csv"), header = T, sep = ";")
+dfcodes = dfcodes[dfcodes$CpG %in% str_replace(names(df_CpG_k), "_k_", "_"),]
+
+
+#################################
+# qualitative description of OXTR
+
+# plot methylation across CpGs to check for sufficient variance
+boxplot(df_CpG_k, range = 1.5)
+
+# count/plot number of outliers per CpG (3*IQR)
+out_count = as.data.frame(matrix(nrow = ncol(df_CpG_k), ncol = 2, dimnames = list(NULL, c("CpG", "nOut"))))
+out_count$CpG = str_replace(names(df_CpG_k), "_k_", "_")
+for(i in 1:ncol(df_CpG_k)){out_count[i,2] = length(boxplot.stats(df_CpG_k[,i], coef = 3)$out)}
+df_out = merge(out_count, dfcodes, by = "CpG", sort = F)
+ggplot(data = df_out, aes(x = reorder(CpG, 1:nrow(df_out)), y = nOut, colour = segment2)) + 
+  geom_point()
+
+
+df_CpGwiseSDs <- as.numeric(read.csv2(here("Data", "OXTRStabwSingleCpGs.csv"))[,3])^2
+df_CpGwiseSDs <- c(df_CpGwiseSDs, rep(0.0228^2, times = ncol(df_CpG_k)-length(df_CpGwiseSDs)))
+dfchi <- nrow(df_CpG_k)-1
+CpGvariancePs <- numeric(ncol(df_CpG_k))
+
+for(i in 1:ncol(df_CpG_k)){
   
-  (df_predExpr$geneExpr)
+  chi_square <- dfchi*var(df_CpG_k[,i])/df_CpGwiseSDs[i]
+  CpGvariancePs[i] = pchisq(chi_square, df = dfchi, lower.tail = F)
   
-  GeneExpr_out <- as.data.frame(matrix(nrow = nfolds*repeats, ncol = 2))
-  GeneExpr_out_foldwise <- as.data.frame(matrix(nrow = maxcomp, ncol = repeats*nfolds))
-  plsGrid <- expand.grid(ncomp = seq(1, maxcomp))
-  count = 0
-  seed = 1000
-  
-  for(i in 1:repeats){
-    
-    set.seed(seed+count)
-    plsFolds <- createFolds(dfpredExpr_perm$geneExpr, k = nfolds)
-    
-    for(j in 1:nfolds){
-      
-      count = count+1
-      
-      trainSet <- dfpredExpr_perm[-plsFolds[[j]],]
-      testSet <- dfpredExpr_perm[plsFolds[[j]],]
-      
-      set.seed(seed+count)
-      plsOptComp <- train(geneExpr ~ ., 
-                          data = trainSet, 
-                          trControl = fitControl, 
-                          method = "pls", 
-                          metric = "Rsquared",
-                          preProcess = c('scale', 'center'), 
-                          na.action = na.omit,
-                          tuneGrid = plsGrid)
-      
-      Rsquared <- 1 - sum((predict(plsOptComp, newdata = testSet) - testSet$geneExpr)^2)/(var(testSet$geneExpr)*nrow(testSet))
-      GeneExpr_out[count, 1] <- max(Rsquared, 0)
-      GeneExpr_out[count, 2] <- plsOptComp$bestTune$ncomp
-      GeneExpr_out_foldwise[, count] <- plsOptComp$results$Rsquared
-      
-    }
-  }
-  savePerm[h] <- mean(GeneExpr_out[,1], na.rm = T)
-}  
-
-write.csv(savePerm, "plsPerm_all.csv")
-
-t2 <- Sys.time()
-t2-t1
-
-
-
-
-
-t = plsr(geneExpr ~ ., data = trainSet, validation = "CV", segments = 10, ncomp = 7)
-Rsquared <- 1 - sum((predict(t, ncomp = 7, newdata = testSet)[1:14] - testSet$geneExpr)^2)/(var(testSet$geneExpr)*nrow(testSet))
-
-
-p = predict(t, ncomp = 7, newdata = testSet)[1:14]
-
-
-
-plsOptComp
-plot(plsOptComp)
-plsOptComp$results
-
-
-
-
-
-
-
-
-
-
-# sparse partial least squares
-splsGrid <- expand.grid(ncomp = seq(1, length(names(df_dum))-1))
-
-spls <- train(geneExpr ~. , 
-              data = df_predExpr, 
-              trControl = fitControl, 
-              method = "spls", 
-              preProcess = c('scale', 'center'), 
-              na.action = na.omit)
-
-spls$results[spls$results$ncomp == as.numeric(spls$bestTune), ]
-plot(spls, metric = "Rsquared")
-
-
-
-
-
-
-
-
-
-
-### profilanalyse
-
-
-
-
-plot(x = names(df_CpG_m), y = ifelse(CpGvariancePs > 0.05, 1, 0))
-
-
-t = apply(df_CpG_m, 2, var)
-sum(t <= meanSens^2)
-
-varTestFun(df_CpG_m[,1], meanSens^2)
-
-
-varTest(df_CpG_m[,1], alternative = "greater", conf.level = 0.95, 
-        sigma.squared = meanSens^2)
-
-((nrow(df_CpG_m)-1)*var(df_CpG_m[,1]))/meanSens^2
-
-
-
-
-
-
-# corrmatrix total
-names(df_CpG_m) <- substr(names(df_CpG_m), 7, 12)
-CpG_corr = cor(df_CpG_m, use = "pairwise.complete.obs")    
-corrplot(CpG_corr, tl.pos = "td", tl.cex = 0.5, method = "color", type = "upper")
-
-
-CpG_corr = cor(df_CpG_m[, which(names(df_CpG_m) == "143"):which(names(df_CpG_m) == "187")], use = "pairwise.complete.obs")    
-corrplot.mixed(CpG_corr)
-
-
-
-
-
-
-
-
-# methylation profiles: https://bioconductor.org/packages/release/bioc/vignettes/BPRMeth/inst/doc/BPRMeth_vignette.html
-
-# Computational and Statistical Epigenomics
-
-# mclust full
-df_CpG_m_trans = t(df_CpG_m)
-BIC <- mclustBIC(df_CpG_m_trans)
-plot(BIC)
-gmm <- Mclust(df_CpG_m_trans, G = 1:30, x = BIC)
-summary(gmm)
-plot(gmm, what = "BIC")
-
-# mclust sufficient variance on CpGs
-
-df_CpG_m_sens = df_CpG_m[,apply(df_CpG_m, 2, sd) > meanSens]
-
-df_CpG_m_sens_trans = t(df_CpG_m_sens)
-BIC <- mclustBIC(df_CpG_m_trans)
-plot(BIC)
-gmm <- Mclust(df_CpG_m_trans, G = 1:30, x = BIC)
-summary(gmm)
-plot(gmm, what = "BIC")
-
-
-
-# PCA
-
-exon3transl = df_CpG_m_sens[,which(names(df_CpG_m_sens) == "125"):which(names(df_CpG_m_sens) == "193")]
-
-fa.parallel(exon3transl, fa="pc")
-KMO(exon3transl)
-
-corrplot(cor(df_CpG_m_sens[,which(names(df_CpG_m_sens) == "143"):which(names(df_CpG_m_sens) == "193")], use = "pairwise.complete.obs"))
-
-
-
-
-
-# USE BORUTA METHOD FOR FEATURE SELECTION?!
-
-# random forest
-df_rf_m = data.frame(df$Genexp_OXTR_mother, df_CpG_m[which(names(df_CpG_m) == "E1"):which(names(df_CpG_m) == "E21")])
-df_rf_m = df_rf_m[!is.na(df_rf_m$df.Genexp_OXTR_mother), ]
-
-rf_model = cforest(df.Genexp_OXTR_mother ~ ., data = df_rf_m)
-correl <- cor(predict(rf_model, OOB = T), df_rf_m$df.Genexp_OXTR_mother)
-R_squared <- if(correl < 0){0}else{correl^2}
-
-
-
-corrplot.mixed(cor(df_rf_m, use = "pairwise.complete.obs"))
-
-
-
-#################################################################
-######### CODE TESTING
-
-# simulate data
-
-N = 1000
-M = 168
-df_test <- matrix(rnorm(N*M,mean=0,sd=1), N, M)
-weights <- runif(M, -0.5, 0.5)
-
-DV <- df_test%*%weights
-DV <- scale(DV) + rnorm(N, sd = sqrt(5))
-
-df_test <- data.frame(DV, df_test)
-
-summary(lm(DV~., data = df_test))
-
-test_out <- as.data.frame(matrix(nrow = nfolds*repeats, ncol = 2))
-test_out_foldwise <- as.data.frame(matrix(nrow = maxcomp, ncol = repeats*nfolds))
-plsGrid <- expand.grid(ncomp = seq(1, maxcomp))
-count = 0
-
-for(i in 1:repeats){
-  
-  plsFolds <- createFolds(df_test$DV, k = nfolds)
-  
-  for(j in 1:nfolds){
-    
-    count = count+1
-    
-    trainSet <- df_test[-plsFolds[[j]],]
-    testSet <- df_test[plsFolds[[j]],]
-    
-    set.seed(seed+count)
-    plsOptComp <- train(DV ~ ., 
-                        data = trainSet, 
-                        trControl = fitControl, 
-                        method = "pls", 
-                        metric = "RMSE",
-                        preProcess = c('scale', 'center'), 
-                        na.action = na.omit,
-                        tuneGrid = plsGrid)
-    
-    Rsquared <- 1 - sum((testSet$DV - predict(plsOptComp, newdata = testSet))^2)/(var(testSet$DV)*(nrow(testSet)-1))
-    
-    test_out[count, 1] <- max(Rsquared, 0)
-    test_out[count, 2] <- plsOptComp$bestTune$ncomp
-    test_out_foldwise[, count] <- plsOptComp$results$Rsquared
-    
-  }
 }
 
-mean(test_out[,1])
+pf(2.5, 109, 11, lower.tail = F)
+
+sum(CpGvariancePs > 0.05)
+sum(p.adjust(CpGvariancePs, method = "fdr") > 0.05)
+dfvar_plot = data.frame(dfcodes, CpGvariancePs)
+dfvar_plot$insuffVar = ifelse(CpGvariancePs > 0.05, 1, 0)
+
+ggplot(data = dfvar_plot, aes(x = reorder(CpG, 1:nrow(df_out)), y = insuffVar, colour = segment2)) +
+  geom_point()
+
+# continue without CpGs with insufficient variance
+df_CpG_k <- df_CpG_k[, CpGvariancePs <= 0.05]
+dfcodes <- dfcodes[CpGvariancePs <= 0.05, ]
+
+# correlation plot 
+CpG_corr = cor(df_CpG_k, use = "pairwise.complete.obs")
+colnames(CpG_corr) <- NULL
+rownames(CpG_corr) <- substring(rownames(CpG_corr), 7)
+
+pdf(here("Figures", "corrPlot_child.pdf"))
+corrplot::corrplot(CpG_corr, tl.pos = "d", tl.cex = 0.25, method = "color", type = "upper", tl.col = "black")
+dev.off()
+
+
+#################################
+# unsupervised datra-driven description of OXTR
+
+# z score variables
+df_CpG_k_z = scale(df_CpG_k)
+
+
+#################
+# factor analysis
+KMO(df_CpG_k_z)
+fa.parallel(df_CpG_k_z, fa="pc")
+pca_all = principal(df_CpG_k, nfactors=7, rotate="oblimin")
+pca_all # factor intercorrelations neglegible
+pca_all = principal(df_CpG_k, nfactors=7, rotate="varimax")
+pca_all$loadings
+
+
+#################
+# cluster analysis (dbscan)
+
+# transpose data
+df_CpG_k_z_trans = t(df_CpG_k_z)
+
+# determine eps. try k = 3, k = 5 (default), k = 8, k = 12
+dbscan::kNNdistplot(df_CpG_k_z_trans, k =  3)
+abline(h = 5, lty = 2) # for k = 3
+dbscan::kNNdistplot(df_CpG_k_z_trans, k =  5)
+abline(h = 5.8, lty = 2) # for k = 5
+dbscan::kNNdistplot(df_CpG_k_z_trans, k =  8)
+abline(h = 7.7, lty = 2) # for k = 8
+dbscan::kNNdistplot(df_CpG_k_z_trans, k =  12)
+abline(h = 8, lty = 2) # for k = 8
+
+# apply clustering
+res.fpc3 <- fpc::dbscan(df_CpG_k_z_trans, eps = 5, MinPts = 3)
+print(res.fpc3)
+res.fpc5 <- fpc::dbscan(df_CpG_k_z_trans, eps = 5.8, MinPts = 5)
+print(res.fpc5)
+res.fpc8 <- fpc::dbscan(df_CpG_k_z_trans, eps = 7.7, MinPts = 8)
+print(res.fpc8)
+res.fpc12 <- fpc::dbscan(df_CpG_k_z_trans, eps = 8, MinPts = 12)
+print(res.fpc12)
+
+# write.csv(data.frame(res.fpc5$cluster, names(df_CpG_k)), "clusterAssignment.csv") 
+
+# plot
+df_clustMem = data.frame(dfcodes$CpG, dfcodes$segment2, res.fpc3$cluster, res.fpc5$cluster, res.fpc8$cluster, res.fpc12$cluster)
+df_clustMem_long = melt(df_clustMem, id.vars = c("dfcodes.CpG", "dfcodes.segment2"))
+
+ggplot(data = df_clustMem_long, aes(x = reorder(dfcodes.CpG, rep((1:nrow(df_clustMem)), length.out = nrow(df_clustMem_long))), y = value, colour = dfcodes.segment2, shape = variable)) + 
+  geom_jitter(width = 0, height = 0.3) + scale_y_continuous(breaks=c(0, 1, 2)) + scale_x_discrete(breaks=NULL) +
+  xlab("CpG Sites") + ylab("Cluster Assignment") + theme_classic() + theme(legend.title = element_blank())
+ggsave(here("Figures", "DBSCAN_clustMembership_children.pdf"), device = "pdf")
+
+
+#################
+# hierarchical clustering (hclust)
+
+# dendrogram
+dd <- dist(df_CpG_k_z_trans, method = "euclidean")
+hc <- hclust(dd, method = "ward.D2")
+
+fviz_dend(hc)
+fviz_nbclust(df_CpG_k_z_trans, FUN = hcut, method = "wss")
+fviz_nbclust(df, FUN = hcut, method = "silhouette")
+
+pdf(here("Figures", "Dendrogram_children.pdf"))
+fviz_dend(
+  hc,
+  k = 3,
+  horiz = TRUE,
+  rect = TRUE,
+  rect_fill = TRUE,
+  rect_border = "jco",
+  k_colors = "jco",
+  cex = 0.3
+)
+dev.off()
+
+
+clustAssign <- cutree(hc, k = 3)
+
+# proportions of genetic sections represented in the three clusters
+df_hclustAssign <- data.frame(dfcodes, clustAssign) 
+round(table(df_hclustAssign[df_hclustAssign$clustAssign == 1, "segment2"])/sum(clustAssign == 1), 2)
+round(table(df_hclustAssign[df_hclustAssign$clustAssign == 2, "segment2"])/sum(clustAssign == 2), 2)
+round(table(df_hclustAssign[df_hclustAssign$clustAssign == 3, "segment2"])/sum(clustAssign == 3), 2)
+
+
+df_hclustAssign$DBSCANclust <- df_clustMem$res.fpc5.cluster
+df_hclustAssign$hclustRec <- ifelse(df_hclustAssign$clustAssign == 1, 
+                                    0,
+                                    ifelse(df_hclustAssign$clustAssign == 2, 
+                                           1,
+                                           2)
+)
+
+round(sum(df_hclustAssign$DBSCANclust == df_hclustAssign$hclustRec)/nrow(df_hclustAssign), 2)
+
+
+
+
 
